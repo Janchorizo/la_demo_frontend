43a6314763f96f1dfdd24b8bf59c4fbdb25da9ec
Janchorizo <jancho@usal.es>
2021-03-12 23:14:36 +0100

    Undo vendor folder structure flattening.

--- pre_build.sh
+++ pre_build.sh
@@ -2,5 +2,5 @@ mkdir -p dist
 echo "Clearing previous build ..."
 rm -fr dist/*
 echo "Copying static files ..."
-cp -r static/vendor/* dist
+cp -r static/vendor dist
 cp static/index_prod.html dist/index.html
