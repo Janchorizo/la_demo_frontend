b04820fbcbc56c4fb5f7a1f8ae6ede3e3fa36dac
Janchorizo <jancho@usal.es>
2021-03-13 12:28:38 +0100

    Add github banner to app.

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 // external
 import React from 'react';
 // internal
+import {GithubBanner} from 'components/github_banner';
 import 'common/style/theme.css';
 
 export default function App() {
   return (
     <div className='flex five center'>
+      <GithubBanner url='https://github.com/Janchorizo/la_demo_frontend'/>
       <h1 className='half light-bg main-c'>La Demo</h1>
     </div>
   );
