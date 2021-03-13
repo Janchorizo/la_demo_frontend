32f7acbf5d79b7bbbd817e1f4d05b5bc98afd215
Janchorizo <jancho@usal.es>
2021-03-13 10:58:44 +0100

    Change range utility to replicate Python's range behaviour.

--- src/common/helpers/func.js
+++ src/common/helpers/func.js
@@ -2,8 +2,20 @@
  * Returns an iterable ranging from start to end (end not
  * included). If the provided arguments are not valid, an
  * empty array is returned.
+ * If only one positional argument is passed, it is interpreted as
+ * the end of the range, else [start, end, step] are set.
  */
-export function* range(start, end=0, step=1) {
+export function* range(...args) {
+  if (![1, 2, 3].includes(args.length)) {
+    return [];
+  }
+
+  const [start, end] = args.length == 1 ? [0, args[0]] : args;
+  const step = args.length < 3 ? 1 : args[2];
+
+  if (start < end && step < 0) {
+    return [];
+  }
   if (start > end && step > 0) {
     return [];
   }
