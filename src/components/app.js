ca9150d82e67260708e3b9d6359276d0d20331e5
Janchorizo <jancho@usal.es>
2021-03-13 13:39:52 +0100

    Add logo to top banner.

--- src/components/app.js
+++ src/components/app.js
@@ -5,10 +5,27 @@ import {GithubBanner} from 'components/github_banner';
 import 'common/style/theme.css';
 
 export default function App() {
+  const logoStyle = {
+    margin: '.5em',
+    top: 0,
+    zIndex: 211,
+    backgroundPosition: 'center',
+    height: '32px',
+    width: '142px',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundImage: 'url(/logo.png)',
+    backgroundColor: '#fff',
+  };
+
   return (
     <div className='flex five center'>
       <GithubBanner url='https://github.com/Janchorizo/la_demo_frontend'/>
-      <h1 className='half light-bg main-c'>La Demo</h1>
+      <div className='half light-bg'>
+        <a href='#'>
+          <div style={logoStyle}/>
+        </a>
+      </div>
     </div>
   );
 };
