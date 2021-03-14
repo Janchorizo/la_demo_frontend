7f65e0a327a3f52fbe7ac55db64312b54f69309e
Janchorizo <jancho@usal.es>
2021-03-14 09:31:38 +0100

    Add localization for the footer content.

--- src/common/intl/en.js
+++ src/common/intl/en.js
@@ -7,6 +7,19 @@ export default {
   'routes.offer': '/the_offer',
 
   /** Footer content */
+  'footer.title1': 'Using the tecnologies from',
+  'footer.title2': 'The Web',
+  'footer.links_cv': 'Download my CV',
+  'footer.links_web': 'Checkout my personal website',
+  'footer.links_origin': 'The original webpage from',
+  'footer.links_front_github': 'This website in Github',
+  'footer.links_back_github': 'The backend in Github',
+  'footer.aside_me': 'My web',
+  'footer.aside_cv': 'My CV',
+  'footer.bottom_certs': 'Certifications and references',
+  'footer.bottom_priv': 'Privacy y cooKies',
+  'footer.bottom_terms': 'Terms and conditions',
+  'footer.cv_link': 'https://janchorizo.github.io/docs/Rodr%C3%ADguezD%C3%ADazAlejandro_CV_en.pdf',
 
   /** Page content */
   'home.title': 'La Demo',
--- src/common/intl/es.js
+++ src/common/intl/es.js
@@ -7,6 +7,19 @@ export default {
   'routes.offer': '/la_oferta',
 
   /** Footer content */
+  'footer.title1': 'Con la tecnología de',
+  'footer.title2': 'La Web',
+  'footer.links_cv': 'Descarga mi CV',
+  'footer.links_web': 'Echa un vistazo a mi página web',
+  'footer.links_origin': 'La página web original de',
+  'footer.links_front_github': 'Esta página en Github',
+  'footer.links_back_github': 'El backend en Github',
+  'footer.aside_me': 'Mi web',
+  'footer.aside_cv': 'Mi CV',
+  'footer.bottom_certs': 'Certificaciones y referencias',
+  'footer.bottom_priv': 'Privacidad y cooKies',
+  'footer.bottom_terms': 'Términos y condiciones',
+  'footer.cv_link': 'https://janchorizo.github.io/docs/Rodr%C3%ADguezD%C3%ADazAlejandro_CV_es.pdf',
 
   /** Page content */
   'home.title': 'La Demo',
