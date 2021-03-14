c861879dd01e7e1d55915916d1857be5e5d26924
Janchorizo <jancho@usal.es>
2021-03-14 09:57:26 +0100

    Normalize topbar directory structure and exports.

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ export {default as App} from './app';
 export {default as LanguageSelector} from './language_selector';
 export {LocalizedRouter} from './localizedRouter';
 export {LocalizedSwitch} from './localizedSwitch';
-export {TopBar} from './TopBar';
+export {default as TopBar} from './top_bar';
 export {default as GlobalNavLinks} from './global_nav_links';
 export {default as Container} from './container';
 export {default as PageContainer} from './page_container';
--- src/components/top_bar/topbar.js
+++ src/components/top_bar/topbar.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import {useIntl} from 'react-intl';
+// internal
 import {
   GlobalNavLinks,
   LanguageSelector,
 } from 'components';
+import {
+  appStrings,
+  AppLanguage,
+  AppRoute,
+} from 'common/intl';
 import style from './style.module.css';
 
 export default function TopBar() {
+  const {formatMessage, locale} = useIntl();
+  function localizeRouteKey(path) {
+    return `/${locale}` + formatMessage({id: path});
+  }
+  const homePath = localizeRouteKey(AppRoute.Home);
+
   return (
     <React.Fragment>
       <div className={style.top}>
-        <a className={style.logo} href='#'>
+        <a className={style.logo} href={homePath}>
           <div style={{backgroundImage: 'url(/logo.png)'}}/>
         </a>
       </div>
