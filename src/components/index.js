0047633c374c7f6a1106fcb38db36355a494580a
Janchorizo <jancho@usal.es>
2021-03-14 09:54:56 +0100

    Update component index

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,9 @@
 export {default as App} from './app';
-export {LanguageSelector} from './LanguageSelector';
+export {default as LanguageSelector} from './language_selector';
 export {LocalizedRouter} from './localizedRouter';
 export {LocalizedSwitch} from './localizedSwitch';
 export {TopBar} from './TopBar';
-export {default as GlobalNavLinks} from './GlobalNavLinks';
+export {default as GlobalNavLinks} from './global_nav_links';
 export {default as Container} from './container';
 export {default as PageContainer} from './page_container';
+export {default as Footer} from './footer';
