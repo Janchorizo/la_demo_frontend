75a1a5254dca038e71c5ad0b4799cd2475d048d9
Janchorizo <jancho@usal.es>
2021-03-14 19:05:54 +0100

    Fix react routing in Netlify builds.

--- pre_build.sh
+++ pre_build.sh
@@ -4,5 +4,6 @@ rm -fr dist/*
 echo "Copying static files ..."
 cp -r static/vendor dist
 cp static/logo.png dist
+cp static/_redirects dist
 cp -r static/assets dist
 cp static/index_prod.html dist/index.html
