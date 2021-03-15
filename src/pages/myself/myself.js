fef3498fab06361912f230ba75eddb1847f73503
Janchorizo <jancho@usal.es>
2021-03-15 10:32:58 +0100

    Add placeholder content to myself page.

--- src/pages/myself/myself.js
+++ src/pages/myself/myself.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {useIntl} from 'react-intl';
 // internal
 import {
   PageContainer,
+  Container,
 } from 'components';
-// import style from './style.module.css';
+import style from './style.module.css';
 
 
 /**
@@ -12,9 +14,46 @@ import {
  * @return {React.Component}
  */
 export default function Myself() {
+  const {formatMessage} = useIntl();
+
   return (
     <PageContainer>
-      <h3>Myself</h3>
+      <Container id={style['hero']} className={style.section}>
+        <div>
+          <h1>{formatMessage({id: 'myself.hero_title'})}</h1>
+          <h2>{formatMessage({id: 'myself.hero_subtitle'})}</h2>
+        </div>
+        <div id={style['hero-img']}
+          style={{backgroundImage: 'url(/assets/myself_hero.png)'}}>
+        </div>
+      </Container>
+      <Container id={style['hero']} className={style.section}>
+        <div>
+          <h1>{formatMessage({id: 'home.hero_title'})}</h1>
+          <h2>{formatMessage({id: 'home.hero_subtitle'})}</h2>
+        </div>
+        <div id={style['hero-img']}
+          style={{backgroundImage: 'url(/assets/me_thinking.jpg)'}}>
+        </div>
+      </Container>
+      <Container id={style['hero']} className={style.section}>
+        <div>
+          <h1>{formatMessage({id: 'home.hero_title'})}</h1>
+          <h2>{formatMessage({id: 'home.hero_subtitle'})}</h2>
+        </div>
+        <div id={style['hero-img']}
+          style={{backgroundImage: 'url(/assets/me_thinking.jpg)'}}>
+        </div>
+      </Container>
+      <Container id={style['hero']} className={style.section}>
+        <div>
+          <h1>{formatMessage({id: 'home.hero_title'})}</h1>
+          <h2>{formatMessage({id: 'home.hero_subtitle'})}</h2>
+        </div>
+        <div id={style['hero-img']}
+          style={{backgroundImage: 'url(/assets/me_thinking.jpg)'}}>
+        </div>
+      </Container>
     </PageContainer>
   );
 }
