5224cd5df0c6c1875138e4f2bd245c83c133f1fe
Janchorizo <jancho@usal.es>
2021-03-16 16:32:24 +0100

    Add parking grind to parkings page.

--- src/pages/parkings/parkings.js
+++ src/pages/parkings/parkings.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 // internal
 import {
   PageContainer,
+  ParkingGrid,
 } from 'components';
 // import style from './style.module.css';
+
+
+const apiPath = 'http://localhost:8000/api/';
+
+
+function useTiles() {
+  const [tiles, setTiles] = useState(null);
+
+  useEffect(() => {
+    window.fetch(apiPath + 'tiles').then((res) => {
+      res.json().then((jsonRes) => {
+        const tileArray = jsonRes.res;
+        const tilesById = Object.fromEntries(
+          tileArray.map((tile) => [tile.id, tile])
+        );
+        setTiles(tilesById);
+      });
+    })
+  }, []);
+
+  return tiles;
+}
+
+
+function useMaps() {
+  const [maps, setMaps] = useState(null);
+
+  useEffect(() => {
+    window.fetch(apiPath + 'maps').then((res) => {
+      res.json().then((jsonRes) => {
+        const mapArray = jsonRes.res;
+        setMaps(mapArray);
+      });
+    })
+  }, []);
+
+  return maps;
+}
 
 
 /**
@@ -12,9 +51,16 @@ import {
  * @return {React.Component}
  */
 export default function Parkings() {
+  const tiles = useTiles();
+  const maps = useMaps();
+
   return (
     <PageContainer>
-      <h3>Parkings</h3>
+      {!(tiles && maps) ? '' : (
+        <ParkingGrid map={maps[0]}
+          editable={false}
+          tilesById={tiles}/>
+      )}
     </PageContainer>
   );
 }
