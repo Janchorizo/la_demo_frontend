0936cf1028d85ffee9c7c497f5aff2b00c6efcd8
Janchorizo <jancho@usal.es>
2021-03-16 20:29:00 +0100

    Update the editor to handle map editing.

--- src/components/map_editor/editor.js
+++ src/components/map_editor/editor.js
@@ -1,29 +1,30 @@
 import React, {useState} from 'react';
-import PropTypes, { arrayOf } from 'prop-types';
+import {useIntl} from 'react-intl';
+import PropTypes from 'prop-types';
 // internal
 import {
-  ParkingGrid
+  ParkingGrid,
 } from 'components';
 import style from './style.module.css';
 
 
-function useMap(){
-  const defaultLayout = [[],[],[],[],[]];
+function useMap() {
+  const defaultLayout = [[], [], [], [], []];
   defaultLayout.forEach((row, rowIdx) => {
-    for(let i=0; i<5; i++){
+    for (let i=0; i<5; i++) {
       defaultLayout[rowIdx].push({
         id: 'grass',
-        orientation:'north',
-      })
+        orientation: 'north',
+      });
     }
   });
-  const defaultMap = {layout: defaultLayout, name: {es:'', en:''}};
+  const defaultMap = {layout: defaultLayout, name: {es: '', en: ''}};
   const [map, setMap] = useState(defaultMap);
   return [map, setMap];
 }
 
 
-function sendMap(map){
+function sendMap(map) {
 
 }
 
@@ -34,11 +35,13 @@ function sendMap(map){
  * @return {React.Component}
  */
 export default function MapEditor({
-  tilesById
-}){
+  tilesById,
+}) {
+  const {formatMessage} = useIntl();
   const [map, setMap] = useMap();
-  
-  function onEdit(newMap){
+
+  function onEdit(edited) {
+    const newMap = Object.assign({}, edited);
     setMap(newMap);
   }
 
@@ -46,40 +49,41 @@ export default function MapEditor({
     const newName = {
       es: e.target.value,
       en: e.target.value,
-    }
+    };
     setMap({...map, name: newName});
   }
-
-  console.log(tilesById)
 
   return (
     <div id={style.editor}>
       <div id={style.input}>
-        <label for='new-map-name'>Ponle nombre al mapa</label>
-        <input id='new-map-name' value={map.name.es} onChange={onNameChange}/>
+        <label htmlFor='new-map-name'>{formatMessage({id: 'parkings.nameInput'})}</label>
+        <input placeholder='. . .'
+          id='new-map-name'
+          value={map.name.es}
+          onChange={onNameChange}/>
       </div>
       <ParkingGrid tilesById={tilesById} map={map} editable={true} onEdit={onEdit}/>
       <button onClick={() => sendMap(map)}>
-        Guardar el mapa
+        {formatMessage({id: 'parkings.save'})}
       </button>
     </div>
-  )
+  );
 }
 
 MapEditor.propTypes = {
   map: PropTypes.shape({
     name: PropTypes.object,
-    layout: arrayOf(
-      arrayOf(
-        PropTypes.shape({
-          id: PropTypes.string,
-          orientation: PropTypes.oneOf([
-            'north', 'east', 'south', 'west'
-          ]),
-          tile_img: PropTypes.string,
-        })
-      )
-    )
+    layout: PropTypes.arrayOf(
+        PropTypes.arrayOf(
+            PropTypes.shape({
+              id: PropTypes.string,
+              orientation: PropTypes.oneOf([
+                'north', 'east', 'south', 'west',
+              ]),
+              tile_img: PropTypes.string,
+            }),
+        ),
+    ),
   }),
   tilesById: PropTypes.object,
   editable: PropTypes.bool,
